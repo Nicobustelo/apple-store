d5605986c3e1c96c1b4f67929cf57cab1f66fc1c
nicobustelo <nbustelo2002@gmail.com>
2024-04-04 16:04:34 -0300

    Database setup - prisma, aiven, MySQL

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ModalProvider } from "@/providers/modal-provider";
+import { ToasterProvider } from "@/providers/toast-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <ToasterProvider />
         <ModalProvider />
         <body className={inter.className}>{children}</body>
       </html>
