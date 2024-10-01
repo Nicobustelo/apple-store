bd7901b67b8667e9930139a6887a5f91352adcea
nicobustelo <nbustelo2002@gmail.com>
2024-10-01 18:23:13 -0300

    Cards de banner y tutorial agregados

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,7 +1,8 @@
-import { Card, CardHeader } from "@/components/ui/card"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Heading } from "@/components/ui/heading"
 import { Separator } from "@/components/ui/separator"
 import prismabd from "@/lib/prismabd"
+import { BookText, Images } from "lucide-react"
 
 interface DashboardPageProps {
     params: {storeId: string}
@@ -21,6 +22,36 @@ const DashboardPage : React.FC<DashboardPageProps> = async ({
             <div className="flex-1 space-y-4 p-8 pt-6">
                 <Heading title={`Bienvenido a ${store?.name}`} description="Configura cualquier parte de tu tienda desde nuestro panel de adminitración"/>
                 <Separator />
+                <div className="grid gap-4 grid-cols-3">
+                    <Card>
+                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                            <CardTitle className="text-sm font-medium">
+                                Guía Rápida - Tutorial
+                            </CardTitle>
+                            <BookText className="h-4 w-4 text-muted-foreground"/>
+                        </CardHeader>
+                        <CardContent>
+                            <div className="text-2xl font-bold">
+                                Configura tu tienda en 5 minutos
+                            </div>
+                        </CardContent>
+                    </Card>
+                    <a href="https://www.figma.com/design/uAffaztYWtFgUjjwGBQ6zp/Banners?node-id=0-1&node-type=CANVAS&t=JmiCfwtKmBUhzQHW-0">
+                        <Card>
+                            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                <CardTitle className="text-sm font-medium">
+                                    Banners
+                                </CardTitle>
+                                <Images className="h-4 w-4 text-muted-foreground"/>
+                            </CardHeader>
+                            <CardContent>
+                                <div className="text-2xl font-bold">
+                                    Utiliza nuestros banners prediseñados
+                                </div>
+                            </CardContent>
+                        </Card>
+                    </a>
+                </div>
             </div>
         </div>
     )
